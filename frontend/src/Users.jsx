e7f72373f3471157b9cea67bf8049fb59c7b019b
Jack Arthur Harrhy <me@jackharrhy.com>
2021-08-01 02:40:28 -0230

    rendering much more data within Users.jsx

--- frontend/src/Users.jsx
+++ frontend/src/Users.jsx
@@ -6,8 +6,29 @@ const UsersQuery = `
   users {
     edges {
       node {
+        id
         username
         avatarUrl
+        discordAccounts {
+          id
+          username
+          discriminator
+        }
+        githubAccounts {
+          id
+          username
+          organizationMemberships {
+            id
+            role
+            account {
+              username
+            }
+            organization {
+              name
+              displayName
+            }
+          }
+        }
       }
     }
   }
@@ -27,7 +48,45 @@ function Users() {
       <p>Users</p>
       <ul class="list-disc">
         {data.users.edges.map(({ node }) => (
-          <li key={node.username}>{node.username}</li>
+          <li key={node.id} className="pb-4">
+            {node.username}
+            {node.discordAccounts.length > 0 && (
+              <ul className="mt-2 pl-3">
+                Discord:
+                {node.discordAccounts.map(({ id, username, discriminator }) => (
+                  <li key={id}>
+                    {username}#{discriminator}
+                  </li>
+                ))}
+              </ul>
+            )}
+            {node.githubAccounts.length > 0 && (
+              <ul className="mt-2 pl-3">
+                GitHub:
+                {node.githubAccounts.map(
+                  ({ id, username, organizationMemberships }) => (
+                    <li key={id}>
+                      {username}
+                      {organizationMemberships.length > 0 && (
+                        <ul className="mt-2 pl-3">
+                          Organizations:
+                          {organizationMemberships.map(
+                            ({ id, role, account, organization }) => (
+                              <li key={id}>
+                                {role}
+                                {account.username}
+                                {organization.name}
+                              </li>
+                            )
+                          )}
+                        </ul>
+                      )}
+                    </li>
+                  )
+                )}
+              </ul>
+            )}
+          </li>
         ))}
       </ul>
     </div>
