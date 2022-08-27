4c4a43eb251f69a06af6243231c85a77888d9b70
Jack Arthur Harrhy <me@jackharrhy.com>
2022-08-27 00:41:19 -0230

    frontend-ts -> frontend, bye bye old frontend

--- frontend/src/components/DeleteButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function DeleteButton({ onClick }) {
-	return (
-		<button className="btn" onClick={onClick}>
-			<span className="mx-1">x</span>
-		</button>
-	);
-}
--- frontend/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	mode: "jit",
-	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-	variants: {
-		extend: {},
-	},
-	plugins: [],
-};
